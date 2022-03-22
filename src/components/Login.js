998991f9db359b4709deecd9aec64b4ce2fdf5c5
thierryKeller-1 <thierry0keller@gmail.com>
2022-03-22 14:23:42 +0300

    login v2

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,22 +3,31 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className='wrapper'>
-      <div className='title-container'>
-        <span className='title'>Optimus Solution</span>
-      </div>
-      <div className='form'>
-        <div className='row-form-username'>
-          <i className='fas fa-user'></i>
-          <input type='text' className='user-input' placeholder='Username' required></input>
-        </div>
-        <div className='row-form-password'>
-          <i className='fas fa-lock'></i>
-          <input type='password' className='user-input' placeholder='Password' required></input>
-        </div>
-        <div className='row-button'>
-          <i className='fas fa-user'></i>
-          <button type='submit' className='signIn'>Login</button>
+    <div className="App">
+      <div className="login-container">
+        <div className="login-content">
+          <div className="login-title">
+            <span className="title">Optimus Solution</span>
+          </div>
+          <div className="form-container">
+            <div className="form-input">
+              <span className='icon'>
+                <i className='fa fa-user'></i>
+              </span>
+              <span className='separator'>|</span>
+              <input className="input input-username" type="text" placeholder="Username" name="username" autocomplete="off" />
+            </div>
+            <div className="form-input">
+              <span className='icon'>
+                <i className='fa fa-lock'></i>
+              </span>
+              <span className='separator'>|</span>
+              <input className="input input-password" type="password" placeholder="Password" name="password" autocomplete="off" />
+            </div>
+          </div>
+          <div className='submit-btn'>
+            <button typte='button'>Login</button>
+          </div>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals";
+import "font-awesome/css/font-awesome.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
