0573dc2d20c261fc00494c04e8029a3430481ec2
thierryKeller-1 <thierry0keller@gmail.com>
2022-03-22 09:30:52 +0300

    first login version

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,26 @@
+import './Login.css';
 import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='wrapper'>
+      <div className='title-container'>
+        <span className='title'>Optimus Solution</span>
+      </div>
+      <div className='form'>
+        <div className='row-form-username'>
+          <i className='fas fa-user'></i>
+          <input type='text' className='user-input' placeholder='Username' required></input>
+        </div>
+        <div className='row-form-password'>
+          <i className='fas fa-lock'></i>
+          <input type='password' className='user-input' placeholder='Password' required></input>
+        </div>
+        <div className='row-button'>
+          <i className='fas fa-user'></i>
+          <button type='submit' className='signIn'>Login</button>
+        </div>
+      </div>
     </div>
   );
 };
