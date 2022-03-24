cc9ce9189e1b9b4483ce5b45651dc39e32aefa9d
yunisuthn <yunis.thn@gmail.com>
2022-03-24 20:09:29 +0300

    component Accueil

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -21,30 +21,30 @@ const Accueil = () => {
             <div className="col-md-2">
               <div className=" my-action">My Action :</div></div>
             <div className="col-md-8">
-              <table class="table table-my-action">
+              <table class="table ">
                 <tbody>
                   <tr className="thead-color">
-                    <td >ACTION</td>
+                    <td className="bi-cpl">ACTION</td>
                     <td >ITEM</td>
                     <td>PROJECT NAME</td>
                   </tr>
                   <tr className="tr-body">
-                    <td>1</td>
+                    <td className="bi-cpl"><i class="bi bi-plus-circle-fill bi-docmt "></i></td>
                     <td>Document</td>
                     <td>Project lanch</td>
                   </tr>
                   <tr className="tr-body">
-                    <td>2</td>
+                    <td className="bi-cpl"><i class="bi bi-pencil-square bi-usr bi-cpl"></i></td>
                     <td>User</td>
                     <td>Admin changed</td>
                   </tr>
                   <tr className="tr-body">
-                    <td>3</td>
+                    <td className="bi-cpl"><i class="bi bi-trash3 bi-drop-dc "></i></td>
                     <td >Document</td>
                     <td >Document droped</td>
                   </tr>
                   <tr className="tr-body">
-                    <td>4</td>
+                    <td className="bi-cpl"><i class="bi bi-plus-circle-fill bi-drop-dc"></i></td>
                     <td >Document</td>
                     <td>Project lanch</td>
                   </tr>
